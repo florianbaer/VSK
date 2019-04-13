ed9c39173fe7724e93968d7be3e180fceb90e995
Florian Bär <binaryfr3ak@gmail.com>
2019-04-13 13:58:25 +0200

    Create logger viewer project

---
+++ logger-viewer/src/main/java/ch/hslu/vsk/logger/viewer/Program.java
@@ -0,0 +1,24 @@
+package ch.hslu.vsk.logger.viewer;
+
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+
+/**
+ * The Class which starts the program.
+ */
+public final class Program {
+
+    /**
+     * The private constructor for the program.
+     */
+    private Program() {
+    }
+
+    /**
+     * The main method.
+     * @param args The main method arguments.
+     */
+    public static void main(final String[] args) {
+        System.out.println("asdf");
+    }
+}
